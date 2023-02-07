7be8b157d63620c88cd04b4e8348e29450dda3b6
Shubham Jain <shubhamsj062@gmail.com>
2023-02-07 21:08:14 +0530

    last commit3

--- index.js
+++ index.js
@@ -28,33 +28,33 @@ app.get("/", function (req, res) {
 });
 console.log(process.env);
 
-app.post("/", function (req, res) {
+// app.post("/", function (req, res) {
 
-  const query = req.body.cityName;
+//   const query = req.body.cityName;
 
-  const apiKey = process.env.WEATHER_API_KEY;
-  const unit = "metric";
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
-  https.get(url, function (response) {
-    console.log(response.statusCode);
+//   const apiKey = process.env.WEATHER_API_KEY;
+//   const unit = "metric";
+//   const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+//   https.get(url, function (response) {
+//     console.log(response.statusCode);
 
-    response.on("data", function (data) {
-      const weatherData = JSON.parse(data);
-      const temp = weatherData.main.temp;
-      console.log(temp);
-      const weatherDescription = weatherData.weather[0].description;
-      const icon = weatherData.weather[0].icon;
-      const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+//     response.on("data", function (data) {
+//       const weatherData = JSON.parse(data);
+//       const temp = weatherData.main.temp;
+//       console.log(temp);
+//       const weatherDescription = weatherData.weather[0].description;
+//       const icon = weatherData.weather[0].icon;
+//       const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
 
-      res.write("<div><p style='text-align: center;font-size:1.5rem;margin-top:10rem'>The Weather in " + req.body.cityName + " is currently " + `<b style="color:rgb(4,86,238);text-transform:capitalize;">` + weatherDescription + "</b>" + "." + "<p></div>");
-      res.write("<h1 style='text-align: center;'>The temeperature in " + req.body.cityName + " is " + `<span style="color:red;">` + temp + "</span>" + " degree Celsius.</h1>");
-      res.write(`<img src="${imageURL}" width="200" height="200" style="border:2px solid rgb(4, 86, 238);background-color:black;display:block;margin:auto;">`);
-      res.send();
-    })
+//       res.write("<div><p style='text-align: center;font-size:1.5rem;margin-top:10rem'>The Weather in " + req.body.cityName + " is currently " + `<b style="color:rgb(4,86,238);text-transform:capitalize;">` + weatherDescription + "</b>" + "." + "<p></div>");
+//       res.write("<h1 style='text-align: center;'>The temeperature in " + req.body.cityName + " is " + `<span style="color:red;">` + temp + "</span>" + " degree Celsius.</h1>");
+//       res.write(`<img src="${imageURL}" width="200" height="200" style="border:2px solid rgb(4, 86, 238);background-color:black;display:block;margin:auto;">`);
+//       res.send();
+//     })
 
-  })
+//   })
 
-})
+// })
 
 
 
